00eeddde33216bdf1816f36e90191968ebd967c5
Ariela Israel <arielaisrael12@gmail.com>
2023-10-31 23:41:28 -0400

    mock api implementation and ui update

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,13 +2,13 @@
  * Defines the base URL for the API.
  * The default values is overridden by the `API_BASE_URL` environment variable.
  */
-const API_BASE_URL = process.env.API_BASE_URL || "http://localhost:5000";
+const API_BASE_URL = process.env.API_BASE_URL || 'http://localhost:5000';
 
 /**
  * Defines the default headers for these functions to work with `json-server`
  */
 const headers = new Headers();
-headers.append("Content-Type", "application/json");
+headers.append('Content-Type', 'application/json');
 
 /**
  * Fetch `json` from the specified URL and handle error status codes and ignore `AbortError`s
@@ -24,23 +24,46 @@ headers.append("Content-Type", "application/json");
  *  If the response is not in the 200 - 399 range the promise is rejected.
  */
 async function fetchJson(url, options) {
-  try {
-    const response = await fetch(url, options);
+	try {
+		const response = await fetch(url, options);
 
-    if (response.status === 204) {
-      return null;
-    }
+		if (response.status === 204) {
+			return null;
+		}
 
-    const payload = await response.json();
+		const payload = await response.json();
 
-    if (payload.error) {
-      return Promise.reject({ message: payload.error });
-    }
-    return payload.data;
-  } catch (error) {
-    if (error.name !== "AbortError") {
-      console.error(error.stack);
-      return Promise.reject({ message: error.message });
-    }
-  }
+		if (payload.error) {
+			return Promise.reject({ message: payload.error });
+		}
+		return payload.data;
+	} catch (error) {
+		if (error.name !== 'AbortError') {
+			console.error(error.stack);
+			return Promise.reject({ message: error.message });
+		}
+	}
+}
+
+const observations = [];
+
+function nextId() {
+	const uint32 = window.crypto.getRandomValues(new Uint32Array(1))[0];
+	return uint32.toString(16);
+}
+
+export async function createObservation(observation, signal) {
+	const now = new Date().toISOString();
+	const newObservation = {
+		...observation,
+		observation_id: nextId(),
+		created_at: now,
+		updated_at: now,
+	};
+	observations.push(newObservation);
+	return newObservation;
+}
+
+export async function listObservations(signal) {
+	return observations;
 }
